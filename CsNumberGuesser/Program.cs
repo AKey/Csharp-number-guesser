19a9f256d878d88581bd613b8478986e11dce9a8
Any Key <stephan.r.fowler@gmail.com>
2020-08-15 12:00:24 -0400

    Added game replay option

--- CsNumberGuesser/Program.cs
+++ CsNumberGuesser/Program.cs
@@ -15,52 +15,68 @@ namespace CsNumberGuesser
 
             Console.WriteLine($"Hello {inputName}, let's play a game...");
 
-            // Create a new Random object
-            Random random = new Random();
-
-            // Use the Random object to generate a correct number
-            int correctNumber = random.Next(1, 10);
-
-            // Init guess var
-            int guess = 0;
-
-            // Ask user for a number
-            Console.WriteLine("Guess a number between 1 and 10");
-
-            // While the guess is not correct. Our main loop
-            while (guess != correctNumber)
+            while (true)
             {
-                // Get user input
-                string input = Console.ReadLine();
+                // Create a new Random object
+                Random random = new Random();
 
-                // Ohh, simple input validation
-                if (!int.TryParse(input, out guess))
+                // Use the Random object to generate a correct number
+                int correctNumber = random.Next(1, 10);
+
+                // Init guess var
+                int guess = 0;
+
+                // Ask user for a number
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                // While the guess is not correct. Our main loop
+                while (guess != correctNumber)
                 {
-                    // Tell the user they need to enter a number
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Please enter a whole number");
-                    Console.ResetColor();
-                    continue;
+                    // Get user input
+                    string input = Console.ReadLine();
+
+                    // Ohh, simple input validation
+                    if (!int.TryParse(input, out guess))
+                    {
+                        // Tell the user they need to enter a number
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please enter a whole number");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    // Cast to int
+                    guess = Int32.Parse(input);
+
+                    // Check the guess
+                    if (guess != correctNumber)
+                    {
+                        // Alert user they guessed wrong
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("That's not the right number. Try again.");
+                        Console.ResetColor();
+
+                    }
                 }
 
-                // Cast to int
-                guess = Int32.Parse(input);
+                // Output success message
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Correct!");
+                Console.ResetColor();
 
-                // Check the guess
-                if (guess != correctNumber)
+                // Ask to play agin
+                Console.WriteLine("Play Again? [Y/N]");
+
+                // Get answer regardles of case 
+                string answer = Console.ReadLine().ToUpper();
+
+                // User must choose 'N' or game will continue
+                if (answer == "N" || answer == "NO")
                 {
-                    // Alert user they guessed wrong
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("That's not the right number. Try again.");
-                    Console.ResetColor();
-
+                    return;
                 }
+                continue;
             }
-
-            // Output success message
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Correct!");
-            Console.ResetColor();
         }
     }
 }
